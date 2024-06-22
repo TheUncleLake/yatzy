b17b9162b0e33be8bfedeeb440a92a2e19c875f9
TheUncleLake <100882470+TheUncleLake@users.noreply.github.com>
2024-06-22 01:27:49 -0400

    implemented yatzy engine php

--- lab06/app/models/YatzyGame.php
+++ lab06/app/models/YatzyGame.php
@@ -5,10 +5,12 @@ class YatzyGame {
     public $rollNo; // 0, 1, 2, or 3
     public $dice; // array of 5 numbers
     public $keep; // array of indices of $dice
+    public $scoreBox; // filled in score boxes
 
     function __construct() {
         $this->rollNo = 0;
         $this->dice = array();
         $this->keep = array();
+        $this->scoreBox = array();
     }
 }
--- lab06/public/index.php
+++ lab06/public/index.php
@@ -1,15 +1,15 @@
 <?php
 require_once('_config.php');
 
-use Yatzy\{Dice, YatzyGame};
+use Yatzy\{Dice, YatzyGame, YatzyEngine};
 
 $d = new Dice();
 $g = new YatzyGame();
 
 for ($i=1; $i<=5; $i++) {
-  $r = $d->roll();
-  echo "ROLL {$i}: {$r}<br>";
-  array_push($g->dice, $r);
+    $r = $d->roll();
+    echo "ROLL {$i}: {$r}<br>";
+    array_push($g->dice, $r);
 }
 
 $g->rollNo++;
@@ -22,3 +22,23 @@ print_r($g->dice);
 echo "<br>";
 echo "KEEP LIST: ";
 print_r($g->keep);
+
+echo "<br><br>";
+YatzyEngine::init();
+foreach (array_keys(YatzyEngine::$scoreBoxFunctions) as $key) {
+    $g->dice = [$d->roll(), $d->roll(), $d->roll(), $d->roll(), $d->roll()];
+    switch ($key) {
+        case "fours": $g->dice = [4,1,1,1,1]; break;
+        case "fives": $g->dice = [5,5,5,5,5]; break;
+        case "yatzy":
+        case "sixes": $g->dice = [6,6,6,6,6]; break;
+    }
+    $g->scoreBox[$key] = YatzyEngine::calculateScore($g, $key);
+    echo "{$key} = {$g->scoreBox[$key]}" . str_repeat("&emsp;", 3);
+    print_r($g->dice);
+    echo("<br>");
+}
+$tot = YatzyEngine::calculateScoreBonus($g);
+echo "<br>";
+echo "TOTAL SCORE: {$tot["score"]}<br>";
+echo "BONUS: {$tot["bonus"]}";
