c1bdb316d9140ee141124ccbf1c0de7c63044316
TheUncleLake <100882470+TheUncleLake@users.noreply.github.com>
2024-07-19 14:07:49 -0400

    implemented game (partial page) update

--- app/views/script.js
+++ app/views/script.js
@@ -13,15 +13,54 @@
         elem.classList.add("die");
         elem.onclick = async function() {
             $.ajax({type: "GET", url: "api/keep"}).then(function(data) {
-                console.log(data);
+                updateGame(data);
             });
         };
         dice.push(elem);
     }
-    // Load saved game if there is one
+    // Update game based on JSON output (rollNo, dice, keep, scoreBox)
     function updateGame(data) {
-        console.log(data);
+        if (data == null) return;
+        if ("rollNo" in data) {
+            if (data.rollNo >= 3 || data.rollNo > 0 && ("keep" in data) && data.keep.length >= 5)
+                btnRoll.disabled = true;
+            else btnRoll.disabled = false;
+        }
+        if ("dice" in data && data.dice.length == 5) {
+            board.innerHTML = "";
+            // Putting dots on dice
+            for (let i = 0; i < dice.length; i++) {
+                dice[i].innerHTML = "";
+                board.appendChild(dice[i]);
+                dice[i].className = dice[i].className.replace(/(?:^|\s)d\d(?!\S)/g, '');
+                dice[i].classList.add("d" + data.dice[i]);
+                let col = [];
+                if (data.dice[i] >= 4) {
+                    for (let j = 0; j <= 1 + (data.dice[i] % 2); j++) {
+                        col[j] = document.createElement("div");
+                        col[j].classList.add("col");
+                        dice[i].appendChild(col[j]);
+                    }
+                }
+                for (let j = 0; j < data.dice[i]; j++) {
+                    const elem = document.createElement("span");
+                    elem.classList.add("dot");
+                    if (data.dice[i] >= 4)
+                        col[(j + 2) % col.length].appendChild(elem);
+                    else dice[i].appendChild(elem);
+                }
+            }
+        }
+        if ("scoreBox" in data) {
+            for (const [key, val] of Object.entries(data.scoreBox)) {
+                const elem = document.getElementById(key);
+                const score = Object.entries(val)[0];
+                elem.textContent = score[1];
+                elem.style.color = score[0] == 0 ? "red" : score[0] == 1 ? "green" : "darkgoldenrod";
+            }
+        }
     }
+    // Load saved game if there is one
     $.ajax({type: "GET", url: "api/loadgame"}).then(function(data) {
         updateGame(data);
     });
